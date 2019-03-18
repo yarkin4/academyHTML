385c87c333e1facab6a81192bacc5c68ddbcffbd
yarkin4 <48678530+yarkin4@users.noreply.github.com>
2019-03-18 13:47:08 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- <meta charset="utf-8"> -->
+	<title>Академия HTML</title>
+	<link ref="stylesheet" src="css/style.css">
+</head>
+<body>
+	<?
+		#include "header.php"
+	?>
+	
+	<?
+		#include "main.php"
+	?>
+	
+	<?
+		#include "footer.php"
+	?>
+</body>
+</html>
