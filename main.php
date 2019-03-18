979ef2e6149d5aab65417b6420a7a18c8760477e
yarkin4 <48678530+yarkin4@users.noreply.github.com>
2019-03-18 18:02:28 +0200

    Создание main для лендинга. Стандартный каркас
    * Возможны проблемы с интеграцией js в структуру. 
    * Слайдеры не содержат кнопки переключения

--- main.php
+++ main.php
@@ -1,1 +1,89 @@
+<main>
+	<div class="slider-bar">
+	<!-- Slider. Need to include JavaScript for  correct visual effects -->
+	<ul class="slider_img">
+	  <li><img src="img/slide1.jpg" alt="Slider 1"></li>
+	  <li><img src="img/slide2.jpg" alt="Slider 2"></li>
+	  <li><img src="img/slide3.jpg" alt="Slider 3"></li>
+	</ul>
+	<!-- Title of slider-->
+	<h1>Обучение HTML</h1>
+	<b class="title_description">Изучите все популярные теги HTML в удобном формате</b>
+	<p class="title_phrase">Мы собрали около 100 самых известных тегов для верстки сайта</p>
+	<span class="button_registration"><a href="#">Зарегистрироваться</a></span>
+	</div>
 
+	<div class="thems_bar">
+		<h2 class="thems_bar_title">Темы для обучения</h2>
+		<p class="thems_bar_descriotion">Выберите подходящую для вас тему обучения</p>
+
+		<!-- Thems slider. Need to include JavaScript for  correct visual effects -->
+		<ul class="thems_bar_slider">
+			<li>
+				<img src="img/them1.jpg" alt="Them 1">
+				<h3>HTML уровень 1</h3>
+				<p>Для самых новичков</p>
+				<a href="#">Начать</a>
+			</li>
+			<li>
+				<img src="img/them1.jpg" alt="Them 1">
+				<h3>HTML уровень 2</h3>
+				<p>Основы верстки</p>
+				<a href="#">Начать</a>
+			</li>
+			<li>
+				<img src="img/them1.jpg" alt="Them 1">
+				<h3>HTML уровень 3</h3>
+				<p>Продвинутая верстка</p>
+				<a href="#">Начать</a>
+			</li>
+			<li>
+				<img src="img/them1.jpg" alt="Them 1">
+				<h3>HTML уровень 4</h3>
+				<p>HTML5.0 Гуру разметки</p>
+				<a href="#">Начать</a>
+			</li>
+		</ul>
+	</div>
+	
+	<div class="steps_bar">
+		<h2 class="thems_bar_title">Темы для обучения</h2>
+		<p class="thems_bar_descriotion">Выберите подходящую для вас тему обучения</p>
+
+	<!-- Thems slider. Need to include JavaScript for  correct visual effects -->
+	<ul class="steps_bar">
+		<li>
+			<img src="img/them1.jpg" alt="Step 1">
+			<h3>Теория</h3>
+			<p>Только нужная и просто написанная</p>
+		</li>
+		<li>
+			<img src="img/them2.jpg" alt="Step 2">
+			<h3>Тестирование</h3>
+			<p>Интерактивные тесты</p>
+		</li>
+		<li>
+			<img src="img/them3.jpg" alt="Step 3">
+			<h3>Практика</h3>
+			<p>Интерестные задания</p>
+		</li>
+	</ul>
+	</div>
+	
+	<!-- Thems slider. Need to include JavaScript for  correct work -->
+	<ol class="email_subscribe">
+		<li>
+			<h2>Академия HTML</h2>
+		</li>
+		<li>
+			<p>Подпишись на рассылку и получай полезные теги html к себе на почту</p>
+		</li>
+		<li>
+			<input type="text">
+		</li>
+		<li>
+			<button>Отправить</button>
+		</li>
+	</ol>
+	
+</main>
