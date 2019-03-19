6c08f0ca2fb02b2cfcbf62fc79069b3bd7393174
Whitedragon12 <48678571+Whitedragon12@users.noreply.github.com>
2019-03-19 16:26:51 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,23 @@
-<>
+<footer>
+  <b>Html academy</b>
+  <div class="step_inform">
+  <ul class="steps_iform">
+    <li><a href="#">о нас</a></li>
+    <li><a href="#">наши контакты</a></li>
+    <li><a href="#">поддержка</a></li>
+    <li><a href="#">обратная связь</a></li>
+    <li><a href="#">наша команда и выпускники</a></li>
+    <li><a href="#">наши цены</a></li>
+    <li><a href="#">регистрация</a></li>
+    <li><a href="#">онлайн поддержка</a></li>
+    </ul>
+  </div>
+  <div class="poyemet_metods_icon">
+    <p color:orange>Payments Metods</p>
+    <span class="pmaster_cart"><a href="#"></a></span>
+    <span class="visa_cart"><a href="#"></a></span>
+    <span class="webmone_shet"><a href="#"></a></span>
+    <span class="pv_shet"><a href="#"></a></span>
+  </div>
+</footer>
+  
