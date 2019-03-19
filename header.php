b4c3eb5884700db4550c5f8f81b4ce00317eea18
Whitedragon12 <48678571+Whitedragon12@users.noreply.github.com>
2019-03-19 16:06:39 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,21 @@
-<>
+<header>
+<div class="menu-bar">
+  <span class="butten_emcontact"><a href="#">info@htmlacademy</a></span>
+  <span class="butten_mobcontact"><a href="#">359-756-854</a></span>
+  <span class="butten_febcontact"><a href="#"></a></span>
+  <span class="butten_twbcontact"><a href="#"></a></span>
+  <span class="butten_g+bcontact"><a href="#"></a></span>
+  <span class="butten_inbcontact"><a href="#"></a></span>
+  </div>
+  </br>
+  <b>Html academy</b>
+<a href="#"><span color:red>Home</span></a>
+<a href="#">Курсы</a>
+<a href="#">Скидки</a>
+<a href="#">О нас</a>
+<a href="#">Контакты</a>
+  <div class="interactive_bar">
+    <span class="butten_poisk"><a hraf="#"></a></span>
+    <span class="butten_contact"><a hraf="#"></a></span>
+    <span class="butten_bye"><a hraf="#"></a></span>
+  </div>
